8a6c2e82ae64e8ac4b38e4cf03f4a9a6ab5a8135
Fernando Caye <fdcaye@gmail.com>
2020-05-20 16:36:57 +0900

    Ok, 1 hour went by. Pushing it as it is.

---
+++ Assets/Scripts/GameFlowController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameFlowController : MonoBehaviour
+{
+    public GameObject UIPanel;
+    public Text gameOverText;
+
+    void Start()
+    {
+        UIPanel.SetActive(false);    
+    }
+
+    public void PlayerWin()
+    {
+        UIPanel.SetActive(true);
+        gameOverText.text = "YOU WIN";
+    }
+
+    public void PlayerLose()
+    {
+        UIPanel.SetActive(true);
+        gameOverText.text = "YOU LOSE";
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameFlowController gameFlowController;
+    private Rigidbody rb;
+    private Vector3 forceVector;
+    [SerializeField] private float baseSpeed;
+    [SerializeField] private float upwardsImpulse;
+    private bool canJump;
+    private bool isAlive;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        isAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isAlive)
+        {
+            ApplyUpwardImpulse();
+            ApplySidewayMovement();
+        }
+    }
+
+    private void ApplyUpwardImpulse()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            canJump = false;
+            rb.AddForce(new Vector3(0, upwardsImpulse, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void ApplySidewayMovement()
+    {
+        float sideForce = Input.GetAxisRaw("Horizontal");
+        forceVector.x = sideForce * baseSpeed;
+        rb.AddForce(forceVector, ForceMode.Acceleration);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+
+        if(collision.collider.CompareTag("Lava"))
+        {
+            isAlive = false;
+            gameFlowController.PlayerLose();
+        }
+
+        if(collision.collider.CompareTag("Goal"))
+        {
+            isAlive = false;
+            gameFlowController.PlayerWin();
+        }
+    }
+}
